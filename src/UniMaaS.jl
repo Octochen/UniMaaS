8cb26eb458d2f956880f30779a09e70a72789865
Octochen <YuchenZhang9504@163.com>
2025-08-10 23:22:04 +0200

    Initial

--- src/UniMaaS.jl
+++ src/UniMaaS.jl
@@ -1,5 +1,6 @@
 module UniMaaS
 
+include("modelling/modelling.jl")
 include("problem/problem.jl")
 include("util/util.jl")
 
---
+++ test/Test.jl
@@ -0,0 +1,5 @@
+using Pkg
+Pkg.add(url="https://github.com/Octochen/UniMaaS")
+using UniMaaS
+
+UniMaaS.modelling.Six_axis_robotic_arm.robot_arm_initial()
