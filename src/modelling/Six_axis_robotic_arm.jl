3cdb6371c8b6bf367887dacf7ed4c50b76cca053
Octochen <YuchenZhang9504@163.com>
2025-08-10 23:10:03 +0200

    Initial

--- src/modelling/Six_axis_robotic_arm.jl
+++ src/modelling/Six_axis_robotic_arm.jl
@@ -9,17 +9,20 @@ using MeshCat
 using MeshCatMechanisms
 using RigidBodyDynamics
 
-vis = MeshCat.Visualizer()
-# open(vis)  # open the visualizer in a separate tab/window
-render(vis) # render the visualizer here inside the jupyter notebook
+function robot_arm_initial()
+    vis = MeshCat.Visualizer()
+    # open(vis)  # open the visualizer in a separate tab/window
+    render(vis) # render the visualizer here inside the jupyter notebook
 
-urdf = joinpath(@__DIR__, "urdf", "follower.urdf")
-robot = parse_urdf(urdf)
-delete!(vis)
-mvis = MechanismVisualizer(robot, URDFVisuals(urdf), vis)
-set_configuration!(mvis, [0.0, 0.0])
+    urdf = joinpath(@__DIR__, "urdf", "follower.urdf")
+    robot = parse_urdf(urdf)
+    delete!(vis)
+    mvis = MechanismVisualizer(robot, URDFVisuals(urdf), vis)
+    set_configuration!(mvis, [0.0, 0.0])
 
-# state = MechanismState(robot, randn(2), randn(2))
-# t, q, v = simulate(state, 5.0);
-return robot
+    # state = MechanismState(robot, randn(2), randn(2))
+    # t, q, v = simulate(state, 5.0);
+    return robot
+end
+
 end
