278c18f4af55c919d4ae8dd9574fbad253b24d28
Octochen <YuchenZhang9504@163.com>
2025-08-12 03:43:42 +0200

    Initial

--- src/UniMaaS.jl
+++ src/UniMaaS.jl
@@ -3,5 +3,13 @@ module UniMaaS
 include("simulation/simulation.jl")
 include("problem/problem.jl")
 include("util/util.jl")
+using .simulation: *
+export *
+
+using .problem: *
+export *
+
+using .util: *
+export *
 
 end
