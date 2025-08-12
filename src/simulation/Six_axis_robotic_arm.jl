2b97a5072c4b3e56d67dbfe3e58b35faac166ac7
Octochen <YuchenZhang9504@163.com>
2025-08-12 04:33:40 +0200

    Initial

--- src/simulation/Six_axis_robotic_arm.jl
+++ src/simulation/Six_axis_robotic_arm.jl
@@ -7,8 +7,9 @@ function Koch_simulation_model()
     # urdf = joinpath(@__DIR__, "src", "simulation", "urdf", "Koch_v1.1", "Koch_v1.1.urdf")
     robot = RigidBodyDynamics.parse_urdf(Float64, urdf)
     RigidBodyDynamics.remove_fixed_tree_joints!(robot)
+    state = RigidBodyDynamics.MechanismState(robot, zeros(6), zeros(6))
     mvis = MeshCatMechanisms.MechanismVisualizer(robot, MeshCatMechanisms.URDFVisuals(urdf))
-    return robot, mvis
+    return robot, state, mvis
 end
 
 end
--- test/Test.jl
+++ test/Test.jl
@@ -1,20 +1,19 @@
-using Pkg
-Pkg.add(url="https://github.com/Octochen/UniMaaS")
+# using Pkg
+# Pkg.add(url="https://github.com/Octochen/UniMaaS")
 using MeshCat, MeshCatMechanisms, RigidBodyDynamics
 
 using UniMaaS
+using UniMaaS.simulation.joint_controller.time_optimal_joint_controller
 
-import UniMaaS.simulation.joint_controller.time_optimal_joint_controller as TOJC
-
-robot, mvis = UniMaaS.simulation.Six_axis_robotic_arm.Koch_simulation_model()
-state = MechanismState(robot, zeros(6), zeros(6))
+robot, state, mvis = UniMaaS.simulation.Six_axis_robotic_arm.Koch_simulation_model()
 
 # Applying control strategies to obtain optimal trajectories
-t1, q1 = TOJC.time_optimal_joint_trajectory(state, [0, 0, 0, -1.6, 0, 0], [4, 0, 0, -1.6, 0, -0.5], 0.2*ones(6), 0.2*ones(6), 0.1)
-t2, q2 = TOJC.time_optimal_joint_trajectory(state, [4, 0, 0, -1.6, 0, -0.5], [4, 0, 0, -1.6, 0, 0], 0.2*ones(6), 0.2*ones(6), 0.1)
-t3, q3 = TOJC.time_optimal_joint_trajectory(state, [4, 0, 0, -1.6, 0, 0], [0, 0, 0, -1.6, 0, 0], 0.2*ones(6), 0.2*ones(6), 0.1)
-t4, q4 = TOJC.time_optimal_joint_trajectory(state, [0, 0, 0, -1.6, 0, 0], [0, 0, 0, -1.6, 0, -0.5], 0.2*ones(6), 0.2*ones(6), 0.1)
-
+t1, q1 = time_optimal_joint_trajectory(state, [0, 0, 0, -1.6, 0, 0], [4, 0, 0, -1.6, 0, -0.5], 0.2*ones(6), 0.2*ones(6), 0.1)
+t2, q2 = time_optimal_joint_trajectory(state, [4, 0, 0, -1.6, 0, -0.5], [4, 0, 0, -1.6, 0, 0], 0.2*ones(6), 0.2*ones(6), 0.1)
+t3, q3 = time_optimal_joint_trajectory(state, [4, 0, 0, -1.6, 0, 0], [0, 0, 0, -1.6, 0, 0], 0.2*ones(6), 0.2*ones(6), 0.1)
+t4, q4 = time_optimal_joint_trajectory(state, [0, 0, 0, -1.6, 0, 0], [0, 0, 0, -1.6, 0, -0.5], 0.2*ones(6), 0.2*ones(6), 0.1)
 
 # MeshCatMechanisms.animate(mvis, t1, q1; realtimerate = 1.);
+MeshCatMechanisms.animate(mvis, vcat(t1, t2.+t1[end], t3.+t1[end].+t2[end], t4.+t1[end].+t2[end].+t3[end]), vcat(q1,q2,q3,q4); realtimerate = 1.);
+
 MeshCatMechanisms.animate(mvis, vcat(t1, t2.+t1[end], t3.+t1[end].+t2[end], t4.+t1[end].+t2[end].+t3[end]), vcat(q1,q2,q3,q4); realtimerate = 1.);
